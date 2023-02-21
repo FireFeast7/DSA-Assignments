997db44d2a4cf400aa05916bb2563d05a6b69394
FireFeast7 <113030718+FireFeast7@users.noreply.github.com>
2023-02-21 08:13:56 +0530

    Add files via upload

--- first.cpp
+++ first.cpp
@@ -94,18 +94,88 @@ class BST{
             return max(height(root->left),height(root->right))+1;
         }
     }
+    public: void swap(Node *temp){
+        if(temp == NULL){
+            return ;
+        }
+        else{
+            Node *ptr;
+            swap(temp->left);
+            swap(temp->right);
+            ptr = temp->left;
+            temp->left = temp->right;
+            temp->right = ptr;
+        }
+    }
+     public: Node* minvalue(Node *node){
+        Node *current = node;
+        while(current && current->left!=NULL){
+            current = current->left;
+        }
+        return current;
+    }
+    public : Node* del(Node *root, int key)
+    {
+        if(root = NULL){
+            return root;
+        }
+        else if(key < root->data){
+            root->left = del(root->left,key);
+        }
+        else if(key > root->data){
+            root->right = del(root->right,key);
+        }
+        else{
+            if(root->left==NULL && root->right ==NULL){
+                delete root;
+                root= NULL;
+                
+            }
+            else if(root->left==NULL){
+                Node* temp = new Node() ;
+                temp = root;
+                root = root->right;
+                delete temp;
+            }
+             else if(root->right==NULL){
+                Node* temp = new Node() ;
+                temp = root;
+                root = root->left;
+                delete temp;
+            }
+            else{
+                Node *temp = minvalue(root->right);
+                root->data = temp->data;  
+                root->right = del(root->right,temp->data);
+            }
+        }
+        return root;
+    }
 };
 int main(){
     BST btree;
     Node *root = NULL;
-    root = btree.insert(root,15);
     root = btree.insert(root,20);
-    root = btree.insert(root,12);
+    root = btree.insert(root,10);
+    root = btree.insert(root,30);
+    root = btree.insert(root,25);
+    root = btree.insert(root,40);
+    root = btree.insert(root,15);
+    root = btree.insert(root,5);
+    root = btree.insert(root,3);
     btree.preorder(root);
+    cout<<endl;
     btree.inorder(root);
+    cout<<endl;
     int k = btree.maxi(root);
     int m = btree.min(root);
-    cout<<m<<" "<<k;
-    cout<<btree.search(root,23);
+    cout<<" "<<m<<endl<<" "<<k<<endl;
+    cout<<" "<<btree.search(root,23)<<endl;
+//    btree.swap(root);
+//    btree.inorder(root);
+   root = btree.del(root,20);
+    btree.inorder(root);
     return 0;
 }
+
+    
